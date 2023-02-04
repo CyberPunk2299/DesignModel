d5ff6e32d35b877be98601fc97f34370160df284
穆庆霖 <503660181@qq.com>
2023-02-04 14:44:36 +0800

    原型模式

---
+++ src/designmode/prototype/ConcretePrototype.java
@@ -0,0 +1,12 @@
+package designmode.prototype;
+
+/**
+ * @description: 原型子类，用于构建原型
+ * @author: rd_qinglin_mu
+ * @create: 2023-02-04 14:41
+ **/
+public class ConcretePrototype extends Prototype{
+    public ConcretePrototype(String name) {
+        setName(name);
+    }
+}
